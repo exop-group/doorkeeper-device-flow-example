c77a4fc6e3c78a5bfc981457a57845139dc49a8b
Marco Nicola <m.nicola@exop-group.com>
2020-06-22 16:35:36 +0200

    Generate User devise model
    $ rails generate devise User

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  devise_for :users
   use_doorkeeper
 
   root to: 'home#index'
