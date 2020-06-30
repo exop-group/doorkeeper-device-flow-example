802905108dab5a24d4fb8d55ad0e8808687be838
Marco Nicola <m.nicola@exop-group.com>
2020-06-30 16:56:55 +0200

    Enable 'device_code' grant flow in Doorkeeper configuration

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -346,6 +346,7 @@ Doorkeeper.configure do
   #   http://tools.ietf.org/html/rfc6819#section-4.4.3
   #
   # grant_flows %w[authorization_code client_credentials]
+  grant_flows %w[device_code]
 
   # Allows to customize OAuth grant flows that +each+ application support.
   # You can configure a custom block (or use a class respond to `#call`) that must
