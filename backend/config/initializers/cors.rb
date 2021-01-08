76aefb5d792b536c8254b017a0d09ba995366b7f
Marco Nicola <m.nicola@exop-group.com>
2021-01-08 17:12:28 +0100

    Upgrade rails to version 6.1.1

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,7 +1,17 @@
+# Be sure to restart your server when you modify this file.
+
+# Avoid CORS issues when API is called from the frontend app.
+# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
+
+# Read more: https://github.com/cyu/rack-cors
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # This is just for demo purposes! :)
     origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
